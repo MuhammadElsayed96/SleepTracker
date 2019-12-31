6aefb7c99f63a70cf923f329a6152be0913f3923
MuhammadElsayed96 <muhammadelsayed96@yahoo.com>
2019-12-31 23:16:48 +0200

    Add a header to the GridLayout

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -93,10 +93,15 @@ class SleepTrackerFragment : Fragment() {
         })
 
         val manager = GridLayoutManager(activity, 3)
+        manager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int) = when (position) {
+                0 -> 3
+                else -> 1
+            }
+        }
         binding.sleepList.layoutManager = manager
 
         val adapter = SleepNightAdapter(SleepNightAdapter.SleepNightListener { nightId ->
-            //            Toast.makeText(context, "$nightId Clicked!", Toast.LENGTH_SHORT).show()
             sleepTrackerViewModel.onSleepNightClicked(nightId)
         })
 
